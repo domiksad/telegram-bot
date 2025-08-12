d66bbf26dd2012647e98302f78f7a82a923afc19
domiksad <domiksad@gmail.com>
2025-08-12 17:38:59 +0200

    update to moderation actions

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 from dotenv import dotenv_values # for token
 from functools import wraps # admin validation
 from collections import deque # chat history
-from telegram import Update, ChatMemberAdministrator, ChatMemberMember, error
+from telegram import ChatPermissions, Update, ChatMemberAdministrator, ChatMemberMember, error
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 from profanityfilter import ProfanityFilter
 
@@ -89,6 +89,35 @@ def require_admin(func):
             await update.message.reply_text("Only admins can use this command")
     return wrapper
 
+async def get_target_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    target_user = None
+
+    if update.message.reply_to_message:
+        target_user = update.message.reply_to_message.from_user
+
+    elif context.args:
+        arg = context.args[0]
+        try:
+            user_id = int(arg)
+            chat_member = await context.bot.get_chat_member(update.effective_chat.id, user_id)
+            target_user = chat_member.user
+        except ValueError:
+            for entity in update.message.entities or []:
+                if entity.type == "text_mention":
+                    target_user = entity.user
+                    break
+                # elif entity.type == "mention":
+                #     username = update.message.text[entity.offset+1 : entity.offset+entity.length]
+                #     try:
+                #         target_user = await context.bot.get_chat(username)
+                #     except:
+                        # return None
+
+    if not target_user:
+        return None
+
+    chat_member = await context.bot.get_chat_member(update.effective_chat.id, target_user.id)
+    return [target_user, chat_member.status]
 # end of internal logic
 
 # commands
@@ -111,49 +140,47 @@ async def check_permissions(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return await update.message.reply_text(f"Bot status: {bot_member.status}")
 
 @require_permission("can_restrict_members", "can_restrict_members")
-async def kick(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not update.message and update.edited_message:
-        return
+async def moderation_action(update: Update, context: ContextTypes.DEFAULT_TYPE, moderation_action: str):
+    target_user, chat_member_status = get_target_user(update=Update, context=context)
 
-    if update.message.reply_to_message: # is it reply to someone message
-        target_user = update.message.reply_to_message.from_user # if so than get his id
-    elif len(context.args) == 1:
-        try:
-            user_id = int(context.args[0])
-            target_user = await context.bot.get_chat_member(update.effective_chat, user_id)
-            target_user = target_user.user
-        except ValueError:
-            if update.message.entities[1]:
-                entity = update.message.entities[1]
-                if entity.type == "text_mention":
-                    target_user = entity.user
-                elif entity.type == "mention":
-                    username = update.message.text[entity.offset : entity.offset + entity.length]
-                    username = username.lstrip("@")
-                    try:
-                        target_user = await context.bot.get_chat(username)
-                    except:
-                        return await update.message.reply_text("User not found")
-            else:
-                return await update.message.reply_text("Incorrect user id")
-    else:
-        return await update.message.reply_text("Usage: /kick <by reply|username|id|mention>")
+    if chat_member_status == "administrator": 
+        return await update.message.reply_text(f"You can't kick other admins")
+    if chat_member_status == "creator":
+        return await update.message.reply_text(f"You can't kick owner")
 
-    await update.message.reply_text(f"Kicked {target_user.name}")
-
-@require_permission("can_restrict_members", "can_restrict_members")
-async def ban(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"Can ban people")
-
-
-@require_permission("can_restrict_members", "can_restrict_members")
-async def warn(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"Can warn people")
-
-
-@require_permission("can_restrict_members", "can_restrict_members")
-async def mute(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"Can mute people")
+    try:            
+        if moderation_action == "kick":
+            await context.bot.ban_chat_member(chat_id=update.effective_chat.id, user_id=target_user.id)
+            await context.bot.unban_chat_member(chat_id=update.effective_chat.id, user_id=target_user.id)
+            action_text = "kicked"
+        elif moderation_action == "ban":
+            await context.bot.ban_chat_member(chat_id=update.effective_chat.id, user_id=target_user.id)
+            action_text = "banned"
+        elif moderation_action == "mute":
+            await context.bot.restrict_chat_member(chat_id=update.effective_chat.id, user_id=target_user.id,
+                permissions=ChatPermissions(
+                    can_send_messages=False,
+                    can_send_media_messages=False,
+                    can_send_polls=False,
+                    can_send_other_messages=False,
+                    can_add_web_page_previews=False,
+                    can_change_info=False,
+                    can_invite_users=False,
+                    can_pin_messages=False
+                ))
+            action_text = "mutted"
+        elif moderation_action == "warn":
+            action_text = "warned"
+            pass
+        elif moderation_action == "unban":
+            await context.bot.unban_chat_member(chat_id=update.effective_chat.id, user_id=target_user.id)
+            action_text = "unbanned"
+        else:
+            raise Exception(f"Wrong moderation_action {moderation_action}")
+        
+        await update.message.reply_text(f"User {target_user.name} was {action_text} by @{update.message.from_user.name}")
+    except error.BadRequest as e:
+        await update.message.reply_text(e.message)
 
 # admin commands stop
 
@@ -164,11 +191,12 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start); application.add_handler(start_handler)
 
     # admin commands
-    check_permissions_handler = CommandHandler('check_permissions', check_permissions); application.add_handler(check_permissions_handler)
-    kick_handler              = CommandHandler('kick', kick);                           application.add_handler(kick_handler)
-    ban_handler               = CommandHandler('ban', ban);                             application.add_handler(ban_handler)
-    warn_handler              = CommandHandler('warn', warn);                           application.add_handler(warn_handler)
-    mute_handler              = CommandHandler('mute', mute);                           application.add_handler(mute_handler)
+    check_permissions_handler = CommandHandler('check_permissions', check_permissions);                   application.add_handler(check_permissions_handler)
+    kick_handler              = CommandHandler('kick', lambda u, c: moderation_action(u, c, "kick"));     application.add_handler(kick_handler)
+    ban_handler               = CommandHandler('ban', lambda u, c: moderation_action(u, c, "ban"));       application.add_handler(ban_handler)
+    warn_handler              = CommandHandler('warn', lambda u, c: moderation_action(u, c, "warn"));     application.add_handler(warn_handler)
+    mute_handler              = CommandHandler('mute', lambda u, c: moderation_action(u, c, "mute"));     application.add_handler(mute_handler)
+    unban_handler             = CommandHandler('unban', lambda u, c: moderation_action(u, c, "unban"));   application.add_handler(unban_handler)
 
     # censorship
     messages_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), profanity_filter); application.add_handler(messages_handler) # its decorator now
